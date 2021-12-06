d37e0446469d2ea502f3469e9a7798fc2d0a1d51
Alex Marles Alcaraz <alex.marles@king.com>
2021-12-06 13:47:42 +0100

    Re-refactor Day 06

--- day06/day06.js
+++ day06/day06.js
@@ -2,31 +2,17 @@
 const { getInputData } = require('../utils');
 
 function theCycleOfLife (data, days) {
-    const lanternfish = [];
+    const lanternfish = Array(9).fill(0);
     data[0].split(',').map(n => parseInt(n, 10)).forEach(fish => {
-        const index = lanternfish.findIndex(group => group.days === fish);
-        if (index > -1) {
-            lanternfish[index].amount = lanternfish[index].amount + 1;
-        } else {
-            lanternfish.push({ days: fish, amount: 1 });
-        }
+        lanternfish[fish] = lanternfish[fish] + 1;
     });
     for (let i = 0; i < days; i++) {
-        let newBorns = 0;
-        lanternfish.forEach((group, i) => {
-            if (group.days > 0) {
-                lanternfish[i].days = lanternfish[i].days - 1;
-            } else {
-                lanternfish[i].days = 6;
-                newBorns += lanternfish[i].amount;
-            }
-        });
-        if (newBorns > 0) {
-            lanternfish.push({ days: 8, amount: newBorns });
-        }
+        const finished = lanternfish.shift();
+        lanternfish[6] = lanternfish[6] + finished;
+        lanternfish.push(finished);
     }
 
-    return lanternfish.reduce((total, current) => total + current.amount, 0);
+    return lanternfish.reduce((total, current) => total + current, 0);
 }
 
 function day06A (file) {
