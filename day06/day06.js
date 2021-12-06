aedaf9303e13aa78ad9c9354c770a79800257f7a
Alex Marles Alcaraz <alex.marles@king.com>
2021-12-06 13:29:15 +0100

    Refactor day 06

--- day06/day06.js
+++ day06/day06.js
@@ -1,29 +1,7 @@
 /* Day 6 */
-const { getInputData, max, min } = require('../utils');
+const { getInputData } = require('../utils');
 
-function day06A (file) {
-    const data = getInputData(file);
-    const days = 80;
-    let lanternfish = data[0].split(',').map(n => parseInt(n, 10));
-    for (let i = 0; i < days; i++) {
-        let newborns = [];
-        lanternfish = lanternfish.map(fish => {
-            if (fish > 0) {
-                return fish - 1;
-            }
-            newborns.push(8);
-            return 6;
-        });
-        lanternfish = lanternfish.concat(newborns);
-        newborns = [];
-    }
-
-    return lanternfish.length;
-}
-
-function day06B (file) {
-    const data = getInputData(file);
-    const days = 256;
+function theCycleOfLife (data, days) {
     const lanternfish = [];
     data[0].split(',').map(n => parseInt(n, 10)).forEach(fish => {
         const index = lanternfish.findIndex(group => group.days === fish);
@@ -49,6 +27,16 @@ function day06B (file) {
     }
 
     return lanternfish.reduce((total, current) => total + current.amount, 0);
+}
+
+function day06A (file) {
+    const data = getInputData(file);
+    return theCycleOfLife(data, 80);
+}
+
+function day06B (file) {
+    const data = getInputData(file);
+    return theCycleOfLife(data, 256);
 }
 
 module.exports = {
